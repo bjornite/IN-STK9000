8d4b82e45a0ff3682f70f6892bcf8059a4fc9582
Unknown <mat.diaz@free.fr>
2019-09-21 10:10:01 +0200

    new model RandomForestClassifierBanker
    You can launch the TestLendingForForest.py to see the result. I changed a little TestLending.py. That's why i created a new file.

---
+++ Project 1/banker/TestLendingForForest.py
@@ -0,0 +1,104 @@
+import pandas
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from forestbanker import RandomForestClassifierBanker
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+## Set up for dataset
+features = ['checking account balance', 'duration', 'credit history',
+            'purpose', 'amount', 'savings', 'employment', 'installment',
+            'marital status', 'other debtors', 'residence time',
+            'property', 'age', 'other installments', 'housing', 'credits',
+            'job', 'persons', 'phone', 'foreign']
+target = 'repaid'
+df = pandas.read_csv('../data/german.data', sep=' ',
+                     names=features+[target])
+
+numerical_features = ['duration', 'age', 'residence time', 'installment', 'amount', 'duration', 'persons', 'credits']
+quantitative_features = list(filter(lambda x: x not in numerical_features, features))
+X = pandas.get_dummies(df, columns=quantitative_features, drop_first=True)
+encoded_features = list(filter(lambda x: x != target, X.columns))
+
+#norm = 1/X.std()
+
+## Test function
+def test_decision_maker(X_test, y_test, interest_rate, decision_maker):
+    n_test_examples = len(X_test)
+    utility = 0
+
+    ## Example test function - this is only an unbiased test if the data has not been seen in training
+    for t in range(n_test_examples):
+        action = decision_maker.get_best_action(X_test.iloc[t])
+        #print("action")
+        #print(action)
+        good_loan = y_test.iloc[t] # assume the labels are correct
+        ##print("good_loan")
+        ##print(good_loan)
+        duration = X_test['duration'].iloc[t]
+        #print("duration")
+        #print(duration)
+        amount = X_test['amount'].iloc[t]
+        #print("amount")
+        #print(amount)
+        # If we don't grant the loan then nothing happens
+        if (action==1):
+            if (good_loan != 1):
+                utility -= amount
+            else:
+                utility += amount*(pow(1 + interest_rate, duration) - 1)
+        ##print("@@@@@@@@@@@@@@@@@@@@@@utility")
+        ##print(utility)
+    return utility
+
+
+## Main code
+
+
+### Setup model
+#import random_banker # this is a random banker
+#decision_maker = random_banker.RandomBanker()
+
+interest_rate = 0.005
+decision_maker = RandomForestClassifierBanker()
+
+### Do a number of preliminary tests by splitting the data in parts
+
+n_tests = 10
+utility = 0
+for iter in range(n_tests):
+    X_train, X_test, y_train, y_test = train_test_split(X[encoded_features], X[target], test_size=0.2)
+    decision_maker.set_interest_rate(interest_rate)
+    decision_maker.fit(X_train, y_train)
+    utility += test_decision_maker(X_test, y_test, interest_rate, decision_maker)
+    #print(utility)
+print("Mean utility")
+print(utility / n_tests)
+
+ypred = decision_maker.predict(X_test)
+print("Accuracy score")
+print(accuracy_score(y_test, ypred))
+
+#Confusion matrix
+
+y_test = list(y_test)
+for i in range(0, len(y_test)):
+    if y_test[i] == 2:
+        y_test[i] = 0
+conf_mat = confusion_matrix(y_true=list(y_test), y_pred=list(ypred))
+print('Confusion matrix:\n', conf_mat)
+labels = ['Class 0', 'Class 1']
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(conf_mat, cmap=plt.cm.Blues)
+fig.colorbar(cax)
+ax.set_xticklabels([''] + labels)
+ax.set_yticklabels([''] + labels)
+plt.xlabel('Predicted')
+plt.ylabel('Expected')
+plt.show()
+
+
+
+
---
+++ Project 1/banker/forestbanker.py
@@ -0,0 +1,71 @@
+from banker import BankerBase, run
+from random import choice
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils import class_weight
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+class RandomForestClassifierBanker(BankerBase):
+    model = None
+
+    def set_interest_rate(self, interest_rate):
+        self.interest_rate = interest_rate
+
+    def parse_y(self, y):
+        y[np.where(y == 2)] = 0 
+        return y
+    
+    def parse_X(self, X):
+        return X
+
+    def build_forest(self, X, y):
+        model = RandomForestClassifier(n_estimators=130)
+        return model
+
+    def get_proba(self, X):
+        ##print("proba")
+        ##print(self.model.predict_proba(np.array(X).reshape(1,-1))[0][1])
+        return self.model.predict_proba(np.array(X).reshape(1,-1))[0][1]
+
+    def expected_utility(self, X):
+        p = self.get_proba(self.parse_X(X))
+        gain = self.calculate_gain(X)
+        ##print("Gain")
+        ##print(gain)
+        ##print("Gain x p")
+        ##print(gain*p)
+        ##print("perte seche")
+        ##print(X['amount'])
+        ##print("argent a perdre")
+        ##print(X['amount']*(1-p.flatten()))
+        expected_utilitiy = gain*p.flatten()-X['amount']*(1-p.flatten())
+        ##print("expected_utilitiy")
+        ##print(expected_utilitiy)
+        return expected_utilitiy
+    
+    def calculate_gain(self, X):
+        ##print("calculate_gain")
+        ##print(X['amount']*((1 + self.interest_rate)**(X['duration']) - 1))
+        return X['amount']*((1 + self.interest_rate)**(X['duration']) - 1)
+        
+    def fit(self, X, y):
+        y = self.parse_y(y.values.reshape(-1,1).ravel())
+        X = self.parse_X(X)
+        self.model = self.build_forest(X, y)
+        self.model.fit(X,y)
+
+    def get_best_action(self, X):
+        actions = (self.expected_utility(X) > 0).astype(int).flatten()
+        actions[np.where(actions == 0)] = 2
+        ##print('action')
+        ##print(actions)
+
+        return actions
+
+    def predict(self,Xtest):
+        return self.model.predict(Xtest)
+
+if __name__ == '__main__':
+    run()
