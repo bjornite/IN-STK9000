ae8f7862296f05b3bcd6e29119a228947ae0636b
Unknown <mat.diaz@free.fr>
2019-10-16 14:38:24 +0200

    Auto stash before merge of "master" and "origin/master"

--- Project 1/banker/finalrandombanker.py
+++ Project 1/banker/finalrandombanker.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 stderr = sys.stderr
 sys.stderr = open(os.devnull, 'w')
-
+"""
 # For NeuralBanker
 import tensorflow as tf
 
@@ -25,7 +25,7 @@ from keras.optimizers import RMSprop
 from keras import regularizers
 sys.stderr = stderr
 
-
+"""
 # For kNNbanker
 from sklearn import metrics
 from sklearn.pipeline import make_pipeline
@@ -325,7 +325,84 @@ class RandomForestClassifierBanker(BankerBase):
         return importance
         print(importance)
 
+# This class give the best parameters for our model.
+class RandomForestClassifierOptimization(BankerBase):
+    model = None
 
+    def __init__(self):
+        self.interest_rate = None
+
+    def set_interest_rate(self, interest_rate):
+        self.interest_rate = interest_rate
+
+    def parse_y(self, y):
+        y[np.where(y == 2)] = 0
+        return y
+
+    def parse_X(self, X):
+        return X
+
+    def build_forest(self, X, y):
+        #model = RandomForestClassifier(n_estimators=130)
+
+        param_grid = {
+            'n_estimators': np.linspace(10, 200).astype(int),
+            'max_depth': [None] + list(np.linspace(3, 20).astype(int)),
+            'max_features': ['auto', 'sqrt', None] + list(np.arange(0.5, 1, 0.1)),
+            'max_leaf_nodes': [None] + list(np.linspace(10, 50, 500).astype(int)),
+            'min_samples_split': [2, 5, 10],
+            'bootstrap': [True, False]
+        }
+
+        # Estimator for use in random search
+        estimator = RandomForestClassifier()
+
+        # Create the random search model
+        model = RandomizedSearchCV(estimator, param_grid, n_jobs = -1,
+                                scoring = 'roc_auc', cv = 3,
+                                n_iter = 10, verbose = 1)
+
+        return model
+
+
+    def expected_utility(self, X):
+        p = self.get_proba(self.parse_X(X))
+        gain = self.calculate_gain(X)
+        expected_utilitiy = (gain*p.flatten())-(X['amount']*(1-p.flatten()))
+        return expected_utilitiy
+
+    def calculate_gain(self, X):
+        return X['amount']*((1 + self.interest_rate)**(X['duration']) - 1)
+
+    def fit(self, X, y):
+        y = self.parse_y(y.values.reshape(-1,1).ravel())
+        X = self.parse_X(X)
+        self.model = self.build_forest(X, y)
+        self.model.fit(X,y)
+
+    def get_proba(self, X):
+        return best_model.predict_proba(np.array(X).reshape(1,-1))[:,1]
+
+    def best_model(self):
+        print(best_model)
+        return self.model.best_estimator_
+
+    def get_best_action(self, X):
+        actions = (self.expected_utility(X) > 0).astype(int).flatten()
+        actions[np.where(actions == 0)] = 2
+        return actions
+
+    def predict(self,Xtest):
+        return self.best_model.predict(Xtest)
+
+    #def predict_proba(self, Xtest):
+        #return self.model.predict_proba(Xtest)
+        #deze staat hierboven al als get_proba
+
+    def get_importances(self, X):
+        importance = list(zip(X, self.model.feature_importances_))
+        return importance
+        print(importance)
 # This function take as parameters an array X_one_column with the corresponding column that we want to anonymize.
     # For instance X['age']. It wiil return the new array with interval of value and not numérical value.
 def privacy_step(X_one_column):
